036a6823d115bba608c642e0366fc1abe18327d7
Jason-B-Jiang <79412167+Jason-B-Jiang@users.noreply.github.com>
2022-01-09 18:12:14 -0500

    Delete DNA_to_RNA directory

--- DNA_to_RNA/DNA_to_RNA.py
+++
@@ -1,7 +0,0 @@
-def DNA_to_RNA(dna: str) -> str:
-    return dna.replace('T', 'U')
-
-if __name__ == "__main__":
-    with open("rosalind_rna.txt", 'r') as f:
-        dna_string = f.readlines()[0].strip()
-        print(DNA_to_RNA(dna_string))
